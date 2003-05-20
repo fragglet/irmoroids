0ee662941ef385b1a1a8b6310ebd1fcd0b63332b
Simon Howard <sdh300@ecs.soton.ac.uk>
2003-05-20 16:36:57 +0000

    Autoconfise

--- common/config.h
+++ common/config.h
@@ -1,8 +1,6 @@
 #ifndef COMMON_CONFIG_H
 #define COMMON_CONFIG_H
 
-#define INSTALL_ROOT "/home/sdh300/asteroids"
-
 #define INTERFACES_DIR INSTALL_ROOT "/interfaces"
 #define MODELS_DIR INSTALL_ROOT "/models"
 
@@ -12,3 +10,6 @@
 #endif /* #ifndef COMMON_CONFIG_H */
 
 // $Log: not supported by cvs2svn $
+// Revision 1.1.1.1  2003/03/17 17:59:28  sdh300
+// Initial import
+//
